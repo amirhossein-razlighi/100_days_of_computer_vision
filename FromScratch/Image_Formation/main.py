5741d11e9eb7902bd267c22f2806ab436dfa6cbd
AmirHossein-nr <arazlighi@gmail.com>
2024-04-09 00:55:23 +0330

    reformat and add documentation

--- FromScratch/Image_Formation/main.py
+++ FromScratch/Image_Formation/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 def get_camera_matrix(focal_length, sensor_size, image_size):
@@ -14,7 +15,9 @@ def get_camera_matrix(focal_length, sensor_size, image_size):
 
 def get_projection_matrix(camera_matrix, rotation_matrix, translation_vector):
     # camera_matrix: np.array, rotation_matrix: np.array, translation_vector: np.array
-    return np.dot(camera_matrix, np.hstack((rotation_matrix, translation_vector[:, np.newaxis])))
+    return np.dot(
+        camera_matrix, np.hstack((rotation_matrix, translation_vector[:, np.newaxis]))
+    )
 
 
 if __name__ == "__main__":
@@ -31,14 +34,12 @@ if __name__ == "__main__":
 
     X = np.loadtxt("./box.xyz")  # Load 3D points
 
-    # Create 3D scatter plot for the 3D points
     fig = go.Figure(
         data=[go.Scatter3d(x=X[:, 0], y=X[:, 1], z=X[:, 2], mode="markers")]
     )
 
     # Add camera poses and their viewing direction to the 3D scatter plot
     for i, (pos, ori) in enumerate(zip(cam_pos, cam_ori)):
-        # Add camera position
         fig.add_trace(
             go.Scatter3d(
                 x=[pos[0]],
@@ -64,16 +65,18 @@ if __name__ == "__main__":
             )
         )
 
-    # Set the title and axis labels
     fig.update_layout(
         scene=dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Z"),
         title="3D Scene and Camera Poses",
     )
-
-    # Show the 3D scatter plot
     fig.show()
 
     K = get_camera_matrix(f, (36, 27), image_res)
+    subs = make_subplots(
+        rows=len(cam_pos),
+        cols=3,
+        column_widths=[0.4, 0.4, 0.2],
+    )
     for i, (pos, ori) in enumerate(zip(cam_pos, cam_ori)):
         R_matrix = R.from_euler("xyz", ori, degrees=True).as_matrix()
         R_ = R_matrix.T
@@ -90,6 +93,15 @@ if __name__ == "__main__":
             if 0 <= x[1, c] < image_res[1] and 0 <= x[0, c] < image_res[0]:
                 img[int(x[1, c]), int(x[0, c])] = 255
 
-        fig = go.Figure(data=[go.Heatmap(z=img, colorscale="gray", zmin=0, zmax=255)])
-        fig.update_layout(title=f"Image {i+1}")
-        fig.show()
+        fig_2d = go.Figure(
+            data=[go.Heatmap(z=img, colorscale="gray", zmin=0, zmax=255)]
+        )
+        subs.add_trace(fig_2d.data[0], row=i+1, col=2)
+        subs.add_trace(
+            go.Scatter(x=x[0], y=x[1], mode="markers", marker=dict(size=5)),
+            row=i+1,
+            col=1,
+        )
+
+    subs.update_layout(title_text="2D Image Formation")
+    subs.show()
---
+++ FromScratch/README.md
@@ -0,0 +1,14 @@
+# From Scratch Projects
+In these projects, I implemented some of famous algorithms used in Computer Vision and 3D Computer vision from scratch. These are obviously for educational purposes and for understanding how things work under the hood.
+
+## Image Formation
+Here, you can see a simple implementation of how an image is formed in a camera (pinhole camera model). The code is written in Python and uses Plotly for visualization. You can find the code in the `Image_Formation` directory. Use the following command to run the code:
+```bash
+pip install plotly numpy scipy
+python main.py
+```
+
+The result will look like this:
+![Image Formation 3D Plot](../Images/formation_3d.png)
+![Image Formation 2D Plot](../Images/formation_2d.png)
+
